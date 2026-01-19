2f6a725c65d554b4dd7b87cf8f82d5be34ef21c4
Yaqeen Alqarra <y422647487@gmail.com>
2026-01-19 11:30:08 +0200

    initial commit - empty components

--- ai_rev/src/comoponents/navbar.jsx
+++ ai_rev/src/comoponents/navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import '../App.css'
+const links=[
+  {name: "Home", href: "#home"},
+  {name: "AI Types", href: "#types"},
+  {name: "Benefits", href: "#benefits"},
+  {name: "Contact", href: "#contact"}
+]
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className='bg-white/80 backrob-blur-md shadow-sm fixed w-full z-50'>
+      <div className='app-container flex justify-between items-center h-16'>
+        {/* logo */ }
+        <span className="text-2xl font-bold bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent ">AI Revolution</span>
+        {/* desktop links */}
+      <div className="hidden md:flex item-center space-x-8">
+        {/*make loop for links */}
+        {links.map((links)=>(
+          <a key={links.name} href={links.href} className="text-gray-500 hover:text-primary px-3 py-2 text-sm font-medium">
+            {links.name}
+          </a>
+        ))}
+        <a href="" className="bg-primary text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-primary/90 transition">Get Started</a>
+      </div>
+
+      
+      </div>      
+    </nav>
+    )
 }
 
 export default Navbar
